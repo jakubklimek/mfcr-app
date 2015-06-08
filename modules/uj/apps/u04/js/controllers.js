1824d0e3566ef407271991f33ee6dc500b0ddaad
Jakub Klímek <klimek@ksi.mff.cuni.cz>
2015-06-08 16:11:38 +0200

    Fixed labels and visualizations

--- modules/uj/apps/u04/js/controllers.js
+++ modules/uj/apps/u04/js/controllers.js
@@ -9,7 +9,7 @@
                 AppService.getData($scope, 'uj', 'uj-detail', {'resource': $scope.resource})
                     .then(function(data) {
                         $scope.ucetniJednotka = data["@graph"][0];
-						$scope.vizuri = $sce.trustAsResourceUrl("http://live.payola.cz:29080/api/v1/datacube/custom/55/b5db0f77dbfdb55a9c8c05f8939b13d0?view=chart&chartType=column&isPolar=false&dimensionUri=http://linked.opendata.cz/ontology/application/mfcrapp/rozpocty/refArea&valueUri=" + $scope.ucetniJednotka.okres[0]['@id']);
+						$scope.vizuri = $sce.trustAsResourceUrl("http://live.payola.cz:29080/api/v1/datacube/custom/55/4fdc1df495657c14fb3dea9d561c5320?view=chart&chartType=column&isPolar=false&dimensionUri=http://linked.opendata.cz/ontology/application/mfcrapp/rozpocty/refArea&valueUri=" + $scope.ucetniJednotka.okres[0]['@id']);
 						AppService.setTitle("Účetní jednotka " + $scope.ucetniJednotka["prefLabel"]);
                     });
             };
--- modules/uj/apps/u08/js/controllers.js
+++ modules/uj/apps/u08/js/controllers.js
@@ -9,7 +9,7 @@
                 AppService.getData($scope, 'uj', 'uj-detail', {'resource': $scope.resource})
                     .then(function(data) {
                         $scope.ucetniJednotka = data["@graph"][0];
-						$scope.vizuri = $sce.trustAsResourceUrl("http://live.payola.cz:29080/api/v1/datacube/custom/51/e6544a4c3bb76c2d4e5e6f4c06e408d7?view=chart&chartType=column&isPolar=false&dimensionUri=http://linked.opendata.cz/ontology/application/mfcrapp/rozpocty/refArea&valueUri=" + $scope.ucetniJednotka.okres[0]['@id']);
+						$scope.vizuri = $sce.trustAsResourceUrl("http://live.payola.cz:29080/api/v1/datacube/custom/51/a508c4ecd131491da921f5df77393fcf?view=chart&chartType=column&isPolar=false&dimensionUri=http://linked.opendata.cz/ontology/application/mfcrapp/rozpocty/refArea&valueUri=" + $scope.ucetniJednotka.okres[0]['@id']);
 						AppService.setTitle("Účetní jednotka " + $scope.ucetniJednotka["prefLabel"]);
                     });
             };
